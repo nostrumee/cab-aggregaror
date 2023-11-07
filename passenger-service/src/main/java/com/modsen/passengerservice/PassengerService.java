6ae9aa86294a1867f4636c65a7f95ba15e558242
Eduard Klygunov <nostrumee@gmail.com>
2023-11-07 19:45:03 +0300

    add dependencies and basic config

--- passenger-service/src/main/java/com/modsen/passengerservice/PassengerService.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/PassengerService.java
@@ -1,4 +1,11 @@
 package com.modsen.passengerservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PassengerService {
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerService.class, args);
+    }
 }
