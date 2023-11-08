9927dc9e25451d2587e9b209f4adb4b14fb1ad81
Eduard Klygunov <nostrumee@gmail.com>
2023-11-08 16:03:09 +0300

    minor fix dto

--- passenger-service/src/main/java/com/modsen/passengerservice/dto/PassengerListResponse.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/dto/PassengerListResponse.java
@@ -1,10 +1,8 @@
 package com.modsen.passengerservice.dto;
 
-import lombok.AllArgsConstructor;
-
 import java.util.List;
 
-@AllArgsConstructor
-public class PassengerListResponse {
-    private List<PassengerResponse> passengers;
+public record PassengerListResponse (
+        List<PassengerResponse> passengers
+) {
 }
