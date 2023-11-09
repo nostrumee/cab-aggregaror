746866184c348f1ae4e5b562b3624c71befb460b
Eduard Klygunov <nostrumee@gmail.com>
2023-11-09 16:37:46 +0300

    add migrations

--- passenger-service/src/main/java/com/modsen/passengerservice/entity/Passenger.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/entity/Passenger.java
@@ -3,10 +3,7 @@ package com.modsen.passengerservice.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -18,6 +15,10 @@ public class Passenger {
     private Long id;
     private String firstName;
     private String lastName;
+
+    @Column(unique = true)
     private String email;
+
+    @Column(unique = true)
     private String phone;
 }
