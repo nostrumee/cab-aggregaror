1b6f46e5a0fc208a2ad7a2152a6d783540ee3bba
Eduard Klygunov <nostrumee@gmail.com>
2023-11-12 20:33:05 +0300

    add controller

---
+++ driver-service/src/main/java/com/modsen/driverservice/controller/DriverController.java
@@ -0,0 +1,147 @@
+package com.modsen.driverservice.controller;
+
+import com.modsen.driverservice.dto.request.CreateDriverRequest;
+import com.modsen.driverservice.dto.request.UpdateDriverRequest;
+import com.modsen.driverservice.dto.response.*;
+import com.modsen.driverservice.service.DriverService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/v1/drivers")
+@RequiredArgsConstructor
+@Tag(name = "Driver Controller", description = "Driver API")
+public class DriverController {
+
+    private final DriverService driverService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get driver page")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Drivers found",
+                    content = {
+                            @Content(schema = @Schema(implementation = DriverPageResponse.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Page number must be equal or greater than 1",
+                    content = {
+                            @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Invalid param",
+                    content = {
+                            @Content(schema = @Schema(implementation = ParamErrorResponse.class))
+                    })
+    })
+    public DriverPageResponse getDriverPage(
+            @RequestParam(required = false, defaultValue = "1") int page,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @RequestParam(name = "order_by", required = false) String orderBy
+    ) {
+        return driverService.getDriverPage(page, size, orderBy);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get a driver by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Driver found",
+                    content = {
+                            @Content(schema = @Schema(implementation = DriverResponse.class))
+                    }),
+            @ApiResponse(responseCode = "404", description = "Driver not found",
+                    content = {
+                            @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    })
+    })
+    public DriverResponse getDriverById(@PathVariable long id) {
+        return driverService.getById(id);
+    }
+
+    @PostMapping
+    @Operation(summary = "Create a driver")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Driver created",
+                    content = {
+                            @Content(schema = @Schema(implementation = DriverResponse.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Invalid data provided",
+                    content = {
+                            @Content(schema = @Schema(implementation = ValidationErrorResponse.class))
+                    }),
+            @ApiResponse(responseCode = "409", description = "Email or phone already taken",
+                    content = {
+                            @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    })
+    })
+    public ResponseEntity<DriverResponse> createDriver(
+            @Valid @RequestBody CreateDriverRequest createRequest,
+            UriComponentsBuilder uriComponentsBuilder
+    ) {
+        DriverResponse response = driverService.addDriver(createRequest);
+        Long driverId = response.id();
+
+        URI location = uriComponentsBuilder
+                .path("api/v1/drivers/{id}")
+                .build(Map.of("id", driverId));
+
+        return ResponseEntity
+                .created(location)
+                .body(response);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update an existing driver")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Driver updated",
+                    content = {
+                            @Content(schema = @Schema(implementation = DriverResponse.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Invalid data provided",
+                    content = {
+                            @Content(schema = @Schema(implementation = ValidationErrorResponse.class))
+                    }),
+            @ApiResponse(responseCode = "404", description = "Driver not found",
+                    content = {
+                            @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    }),
+            @ApiResponse(responseCode = "409", description = "Email or phone already taken",
+                    content = {
+                            @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    })
+    })
+    public DriverResponse updateDriver(
+            @PathVariable long id,
+            @Valid @RequestBody UpdateDriverRequest updateRequest
+    ) {
+        return driverService.updateDriver(updateRequest, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete a driver")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Driver deleted"),
+            @ApiResponse(responseCode = "404", description = "Driver not found",
+                    content = {
+                            @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    })
+    })
+    public void deletePassenger(@PathVariable long id) {
+        driverService.deleteDriver(id);
+    }
+}
--- driver-service/src/main/java/com/modsen/driverservice/dto/request/CreateDriverRequest.java
+++ driver-service/src/main/java/com/modsen/driverservice/dto/request/CreateDriverRequest.java
@@ -1,13 +1,36 @@
 package com.modsen.driverservice.dto.request;
 
-import lombok.Builder;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 
-@Builder
+@Schema(description = "Create driver request")
 public record CreateDriverRequest(
+        @Schema(name = "First name", example = "John")
+        @NotBlank(message = "Driver's first name must be defined")
+        @Length(max = 25, message = "Passenger's first name must be shorter than 25 symbols")
         String firstName,
+
+        @Schema(name = "Last name", example = "Doe")
+        @NotBlank(message = "Driver's last name must be defined")
+        @Length(max = 25, message = "Passenger's last name must be shorter than 25 symbols")
         String lastName,
+
+        @Schema(name = "Licence number", example = "111222333")
+        @NotBlank(message = "Driver's licence number must be defined")
+        @Pattern(message = "Licence number is not valid", regexp = "^\\d{9}$")
         String licenceNumber,
+
+        @Schema(name = "Email", example = "johndoe@example.com")
+        @NotBlank(message = "Driver's email must be defined")
+        @Email(message = "Email is not valid", regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
         String email,
+
+        @Schema(name = "Phone number", example = "123-45-67")
+        @NotBlank(message = "Driver's phone must be defined")
+        @Pattern(message = "Phone number is not valid", regexp = "^\\d{3}-\\d{2}-\\d{2}$")
         String phone
 ) {
 }
