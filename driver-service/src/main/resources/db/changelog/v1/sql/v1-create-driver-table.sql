70c0a248a99e6d36edd16cef3c77f03173b68c18
Eduard Klygunov <nostrumee@gmail.com>
2023-11-20 15:32:29 +0300

    minor fix driver migrations

--- driver-service/src/main/resources/db/changelog/v1/sql/v1-create-driver-table.sql
+++ driver-service/src/main/resources/db/changelog/v1/sql/v1-create-driver-table.sql
@@ -6,6 +6,5 @@ create table driver
     licence_number varchar(10) unique  not null,
     email          varchar(255) unique not null,
     phone          varchar(10) unique  not null,
-    status         varchar(15)         not null default 'AVAILABLE',
-    rating         numeric(3, 2)       not null default 5.0
+    status         varchar(15)         not null default 'AVAILABLE'
 );
