4a374228a186a5aa49753e083a28ab878b3638ff
Eduard Klygunov <nostrumee@gmail.com>
2023-11-30 13:52:25 +0300

    minor refactor

--- driver-service/src/main/java/com/modsen/driverservice/config/RideFunctions.java
+++ driver-service/src/main/java/com/modsen/driverservice/config/RideFunctions.java
@@ -2,13 +2,10 @@ package com.modsen.driverservice.config;
 
 import com.modsen.driverservice.dto.message.AcceptRideMessage;
 import com.modsen.driverservice.dto.message.CreateRideMessage;
-import com.modsen.driverservice.dto.message.UpdateDriverRatingMessage;
-import com.modsen.driverservice.service.DriverService;
 import com.modsen.driverservice.service.RideOrderService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 @Configuration
