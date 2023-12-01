7b08bfbe89d032afc4e9a62a72883a6ff84698a3
Eduard Klygunov <nostrumee@gmail.com>
2023-12-01 20:00:20 +0300

    minor refactor

--- notification-service/src/main/java/com/modsen/notificationservice/service/impl/RideStatusToEmailTransformer.java
+++ notification-service/src/main/java/com/modsen/notificationservice/service/impl/RideStatusToEmailTransformer.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.HashMap;
 import java.util.Map;
 
 import static com.modsen.notificationservice.util.MailParams.*;
