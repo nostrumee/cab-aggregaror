8e194ca4fc900acd3d98f63d5efd0ed5c87f5e86
Eduard Klygunov <nostrumee@gmail.com>
2024-01-08 17:26:30 +0300

    refactor

--- ride-service/src/main/java/com/modsen/rideservice/client/PassengerClient.java
+++ ride-service/src/main/java/com/modsen/rideservice/client/PassengerClient.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import static com.modsen.rideservice.util.UriPaths.*;
 
 @FeignClient(
-        value = "passenger-service",
+        value = "${passenger-service.name}",
+        path = PASSENGER_SERVICE_BASE_PATH,
         configuration = PassengerClientConfig.class
 )
 public interface PassengerClient {
 
-    @GetMapping(PASSENGER_SERVICE_BASE_PATH + GET_BY_ID_PATH)
+    @GetMapping(GET_BY_ID_PATH)
     PassengerResponse getPassengerById(@PathVariable("id") long id);
 }
